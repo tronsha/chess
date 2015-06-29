baec195be6f349f8fc23d8b803ad63e277670c4f
Stefan Hüsges <tronsha@gmx.de>
2015-06-29 22:28:52 +0200

    update king check move castling

--- library/King.php
+++ library/King.php
@@ -34,20 +34,33 @@ class King extends Chessman
         } elseif (round($this->getDistance($to) / M_SQRT2, 3) == 1) {
             return true;
         } elseif ($this->getDistance($to) == 2) {
+            $board = $this->board->getArray();
             $from = $this->getPosition();
             $color = $this->getColor();
             if ($color === 0) {
                 if ($from == [4, 7] && $this->board->key2ids($to) == [6, 7] && strpos($this->board->getCastling(), 'K') !== false) {
+                    if ($board[7][5] !== null || $board[7][6] !== null) {
+                        return false;
+                    }
                     return true;
                 }
                 if ($from == [4, 7] && $this->board->key2ids($to) == [2, 7] && strpos($this->board->getCastling(), 'Q') !== false) {
+                    if ($board[7][1] !== null || $board[7][2] !== null || $board[7][3] !== null) {
+                        return false;
+                    }
                     return true;
                 }
             } elseif ($color === 1) {
                 if ($from == [4, 0] && $this->board->key2ids($to) == [6, 0] && strpos($this->board->getCastling(), 'k') !== false) {
+                    if ($board[0][5] !== null || $board[0][6] !== null) {
+                        return false;
+                    }
                     return true;
                 }
                 if ($from == [4, 0] && $this->board->key2ids($to) == [2, 0] && strpos($this->board->getCastling(), 'q') !== false) {
+                    if ($board[0][1] !== null || $board[0][2] !== null || $board[0][3] !== null) {
+                        return false;
+                    }
                     return true;
                 }
             }
