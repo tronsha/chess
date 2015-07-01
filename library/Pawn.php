9b6d1ed27e53769b45e2ab2478f6fc6fc99d4677
Stefan Hüsges <tronsha@gmx.de>
2015-07-01 09:22:41 +0200

    Pawn checkMove

--- library/Pawn.php
+++ library/Pawn.php
@@ -26,4 +26,47 @@ class Pawn extends Chessman
             return 'p';
         }
     }
+
+    public function checkMove($to)
+    {
+        $fromPos = $this->getPosition();
+        $toPos = $this->board->key2ids($to);
+        $distance = round($this->getDistance($to), 3);
+        $board = $this->board->getArray();
+        if ($distance == 1) {
+            if ($board[$toPos[1]][$toPos[0]] !== null) {
+                return false;
+            }
+            if ($this->color === 0 && $fromPos[1] > $toPos[1]) {
+                return true;
+            }
+            if ($this->color === 1 && $fromPos[1] < $toPos[1]) {
+                return true;
+            }
+        }
+        if ($distance == (round(M_SQRT2, 3))) {
+            if ($board[$toPos[1]][$toPos[0]] === null && $this->board->getEnpassant() != $to) {
+                return false;
+            }
+            if ($this->color === 0 && $fromPos[1] > $toPos[1]) {
+                return true;
+            }
+            if ($this->color === 1 && $fromPos[1] < $toPos[1]) {
+                return true;
+            }
+        }
+        if ($distance == 2) {
+            if ($board[$toPos[1]][$toPos[0]] !== null) {
+                return false;
+            }
+            if ($this->color === 0 && $fromPos[1] == 6 && $toPos[1] == 4) {
+                return true;
+            }
+            if ($this->color === 1 && $fromPos[1] == 1 && $toPos[1] == 3) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
