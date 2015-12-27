1543bb119f7facdfd9dae676a7a06beca06cab93
Stefan Hüsges <tronsha@gmx.de>
2015-12-27 21:52:09 +0100

    moves update

--- public/js/player.js
+++ public/js/player.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function () {
     var $ = jQuery;
+    var urlvars = getUrlVars();
+    var moves = urlvars.moves;
     var move = '';
-    $('#board > div').each(function() {
+    $('#board > div').each(function () {
         var $this = $(this);
-        $this.click(function() {
+        $this.click(function () {
             var $this = $(this);
             if (move == $this.attr('id')) {
                 $this.removeClass('selected');
@@ -12,9 +14,23 @@ jQuery(document).ready(function () {
                 $this.addClass('selected');
                 move += $this.attr('id');
                 if (move.length == 4) {
-                    location.href += '|' + move;
+                    if (moves === undefined) {
+                        window.location.href = window.location.href += (window.location.href.indexOf('?') == -1 ? '?' : '&') + 'moves=' + move;
+                    } else {
+                        window.location.href = window.location.href.replace(moves, moves + '|' + move);
+                    }
                 }
             }
         });
     });
 });
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
